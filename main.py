123b06f76bd4448d3628c3a177a978a4f6f6bf94
handsomepyp <71016684+handsomepyp@users.noreply.github.com>
2022-04-15 16:18:53 +0800

    开始重构代码

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 
+
 if __name__ == '__main__':
-    a = [['c', 'd'], ['c', 'b']]
-    a.sort()
-    print(a)
+    h = {}
+    h['a'] = [1, 2, 3]
+    print(h)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- recommend.py
+++ recommend.py
@@ -1,8 +1,46 @@
 import os
 
+
+def recommendNode(node): # 待推荐流程片段仅为一个节点
+    """
+    待推荐流程片段仅为一个节点
+    :param node:
+    :return:
+    """
+    print(node)
+
+
+def hash_graph(graph):
+    """
+    返回图的哈希值
+    :param graph:
+    :return: 图的哈希值
+    """
+    res = 0
+    for edge in graph:
+        res += hash(tuple(edge))
+    return res
+
+
+def recommendGraph(graph):
+    """
+    待推荐流程片段为一个子图
+    :return:
+    """
+    cur_graph_hash = hash_graph(graph)
+
+    print("hello world")
+
+
 if __name__ == '__main__':
-    a = ['a', 'b']
-    b = ['b', 'a']
-    print(hash(tuple(a)), hash(tuple(b)))
-
-
+    file = open('./processSnippets/process_example')
+    contents = file.readlines()
+    flag = contents[0]
+    if flag == "node\n":
+        recommendNode(contents[1])
+    else:
+        graph = []
+        for content in contents:
+            graph.append(list(content.split()))
+        print(graph)
+        recommendGraph(graph)
